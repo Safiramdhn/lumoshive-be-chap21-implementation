94f2e46b8186b34883d08f217aec9a63476bf90a
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2024-11-04 22:18:15 +0800

    Update todo_model.go

--- models/todo_model.go
+++ models/todo_model.go
@@ -1,10 +1,10 @@
 package models
 
 type Todos struct {
-	ID                  int    `json:"id"`
-	UserID              int    `json:"user_id"`
-	Description         string `json:"description"`
-	TodoStatus          string `json:"todo_status"`
-	TotalTodoNotStarted int    `json:"total_todo_not_started"`
-	TotalTodoDone       int    `json:"total_todo_done"`
+	ID                  int    `json:"id,omitempty"`
+	UserID              int    `json:"user_id,omitempty"`
+	Description         string `json:"description,omitempty"`
+	TodoStatus          string `json:"todo_status,omitempty"`
+	TotalTodoNotStarted int    `json:"total_todo_not_started,omitempty"`
+	TotalTodoDone       int    `json:"total_todo_done,omitempty"`
 }
